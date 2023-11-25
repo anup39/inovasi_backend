236ded670a89d80ea536a06209a619a009a71c7e
Anup Dahal <adahal39@gmail.com>
2023-11-25 16:32:24 +0545

    changes in sql

--- app/functions.sql
+++ app/functions.sql
@@ -6,20 +6,17 @@ AS $$
 DECLARE
     mvt bytea;
     estateids text;
-    actual_supplier text;
 BEGIN
     estateids := trim((query_params::jsonb) ->> 'estateids');
-    actual_supplier := trim((query_params::jsonb)->> 'actual_supplier');
-
     SELECT INTO mvt ST_AsMVT(tile, 'function_zxy_query_app_agriplot_by_estateids', 4096, 'geom') FROM (
         SELECT
-            ST_AsMVTGeom(ST_Transform(ST_CurveToLine(geom), 3857), ST_TileEnvelope(z, x, y), 4096, 64, true) AS geom, id_estate ,actual_supplier
+            ST_AsMVTGeom(ST_Transform(ST_CurveToLine(geom), 3857), ST_TileEnvelope(z, x, y), 4096, 64, true) AS geom, id_estate
         FROM app_agriplot 
-        WHERE  actual_supplier::text = actual_supplier AND id_state::text IN estateids 
+        WHERE id_estate::text ='GEL13244'
     ) AS tile WHERE geom IS NOT NULL;
 
     RETURN mvt;
-END;
+END
 $$
 LANGUAGE plpgsql
 STABLE
