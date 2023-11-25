f4e85329d0aa95e3bcd559664e200ed8c34ff0bc
Anup Dahal <adahal39@gmail.com>
2023-11-25 12:17:36 +0545

    changes in sql querys

--- app/functions.sql
+++ app/functions.sql
@@ -6,10 +6,10 @@ AS $$
 DECLARE
     mvt bytea;
     estateids text;
-    actual_supplier text 
+    actual_supplier text;
 BEGIN
     estateids := trim((query_params::jsonb) ->> 'estateids');
-    actual_supplier:= trim((query_params::jsonb)->> 'actual_supplier');
+    actual_supplier := trim((query_params::jsonb)->> 'actual_supplier');
 
     SELECT INTO mvt ST_AsMVT(tile, 'function_zxy_query_app_agriplot_by_estateids', 4096, 'geom') FROM (
         SELECT
