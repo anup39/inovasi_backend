ab643774e41eba5129232188c0442a84729c81ab
Anup Dahal <adahal39@gmail.com>
2024-01-19 08:48:32 +0000

    fixe

--- app/functions.sql
+++ app/functions.sql
@@ -57,11 +57,11 @@ CREATE OR REPLACE FUNCTION function_zxy_query_app_agriplot_by_millideq_and_statu
 RETURNS bytea
 AS $$
 DECLARE
-    mvt bytea;
-    mill_eq_id text;
+    mvt bytea
+    mill_eq_id text
     statusplot text;
 BEGIN
-    mill_eq_id := trim((query_params::jsonb) ->> 'mill_eq_id'));
+    mill_eq_id := trim((query_params::jsonb) ->> 'mill_eq_id'))
     statusplot := trim((query_params::jsonb) ->> 'status_of_plot'));
     SELECT INTO mvt ST_AsMVT(tile, 'function_zxy_query_app_agriplot_by_millideq_and_status', 4096, 'geom') FROM (
         SELECT
@@ -81,28 +81,26 @@ STABLE
 PARALLEL SAFE;
 COMMENT ON FUNCTION function_zxy_query_app_agriplot_by_millideq_and_status IS 'Filters the Agriplot table by mill eq id and status and renders 2D geometries';
 
-
 CREATE OR REPLACE FUNCTION function_zxy_query_app_agriplot_by_radius_and_status(z integer , x integer ,  y integer , query_params json)
 RETURNS bytea
 AS $$
 DECLARE
     mvt bytea;
-    radius text;
     mill_point text;
     statusplot text;
+    radius text;
 BEGIN
-    radius := trim((query_params::jsonb) ->> 'radius'));
-    mill_point := trim((query_params::jsonb) ->> 'mill_point'));
-    statusplot := trim((query_params::jsonb) ->> 'status_of_plot'));
+    radius := trim((query_params::jsonb) ->> 'radius');
+    statusplot := trim((query_params::jsonb) ->> 'status');
     SELECT INTO mvt ST_AsMVT(tile, 'function_zxy_query_app_agriplot_by_radius_and_status', 4096, 'geom') FROM (
         SELECT
-            ST_AsMVTGeom(ST_Transform(ST_CurveToLine(geom), 3857), ST_TileEnvelope(z, x, y), 4096, 64, true) AS  geom , app_agriplot.id, app_agriplot.id_estate, 
+            ST_AsMVTGeom(ST_Transform(ST_CurveToLine(ST_Force2D(geom)), 3857), ST_TileEnvelope(z, x, y), 4096, 64, true) AS  geom , app_agriplot.id, app_agriplot.id_estate, 
             app_agriplot.ownership_plot, app_agriplot.subsidiary, app_agriplot.estate, app_agriplot.agriplot_id,
             app_agriplot.type_of_supplier, app_agriplot.village, app_agriplot.sub_district, app_agriplot.district,
             app_agriplot.province, app_agriplot.country, app_agriplot.planted_area, app_agriplot.year_update,
             app_agriplot.risk_assess, app_agriplot.ghg_luc, app_agriplot.status_of_plot, app_agriplot.millideq,app_agriplot.def_free,app_agriplot.compliance
         FROM public.app_agriplot 
-        WHERE public.app_agriplot.status_of_plot::text = statusplot AND ST_DWithin(public.app_agriplot.geom, ST_GeomFromText(mill_point, 4326), radius::double precision)
+        WHERE public.app_agriplot.status_of_plot::text= statusplot  AND ST_DWithin(public.app_agriplot.geom, ST_GeomFromText(query_params->>'mill_point', 4326), radius::double precision)
     ) AS tile WHERE geom IS NOT NULL;
     RETURN mvt;
 END
@@ -111,7 +109,6 @@ LANGUAGE plpgsql
 STABLE
 PARALLEL SAFE;
 COMMENT ON FUNCTION function_zxy_query_app_agriplot_by_radius_and_status IS 'Filters the Agriplot table by radius and status and renders 2D geometries';
-
 
 
 
